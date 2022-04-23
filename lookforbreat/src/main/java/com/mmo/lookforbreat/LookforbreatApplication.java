00669265ad0282e96efee2e206245b64a15bb890
Nam1806 <97615450+Nam1806@users.noreply.github.com>
2022-04-23 19:37:47 +0700

    init code

---
+++ lookforbreat/src/main/java/com/mmo/lookforbreat/LookforbreatApplication.java
@@ -0,0 +1,13 @@
+package com.mmo.lookforbreat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookforbreatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookforbreatApplication.class, args);
+	}
+
+}
---
+++ lookforbreat/src/test/java/com/mmo/lookforbreat/LookforbreatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmo.lookforbreat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookforbreatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
